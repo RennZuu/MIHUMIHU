30a942a70e710875c725303d18614f2734b963f1
Xyrenn <122950143+RennZuu@users.noreply.github.com>
2026-03-29 01:56:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something For U, Cantikkuu`.split('')
+const text = `Untukmu, Sayangku`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
